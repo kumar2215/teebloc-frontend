9fc916a8765911970722a8450d1acee4658754d5
Kim Young Jin <kimyoungjin1001@gmail.com>
2025-01-09 12:52:39 +0900

    fix

--- src/components/CreateWorksheet/stripeHook.ts
+++ src/components/CreateWorksheet/stripeHook.ts
@@ -3,22 +3,40 @@ import { useSearch } from "wouter";
 import qs from "qs";
 import { useApolloClient, useQuery } from "@apollo/client";
 import { GET_USER_SUBSCRIPTION } from "../../hooks/data";
+import { useUser } from "@clerk/clerk-react";
 
 export function useStripeReturn() {
   const searchParams = useSearch();
   const [showSuccessModal, setShowSuccessModal] = useState(false);
   const client = useApolloClient();
+  const { user, isLoaded } = useUser();
 
   useEffect(() => {
     const params = qs.parse(searchParams, { ignoreQueryPrefix: true });
     if (params["subscription-success"] === "true") {
       setShowSuccessModal(true);
-      // Refetch subscription status when success is true
-      client.refetchQueries({
-        include: [GET_USER_SUBSCRIPTION],
-      });
+
+      // Wait for user to be loaded before refetching
+      if (!isLoaded) {
+        const checkAndRefetch = setInterval(() => {
+          if (user?.id) {
+            client.refetchQueries({
+              include: [GET_USER_SUBSCRIPTION],
+            });
+            clearInterval(checkAndRefetch);
+          }
+        }, 100);
+
+        // Cleanup the interval if component unmounts
+        return () => clearInterval(checkAndRefetch);
+      } else if (user?.id) {
+        // User is already loaded, refetch immediately
+        client.refetchQueries({
+          include: [GET_USER_SUBSCRIPTION],
+        });
+      }
     }
-  }, [searchParams, client]);
+  }, [searchParams, client, user, isLoaded]);
 
   return { showSuccessModal, setShowSuccessModal };
 }
