c25c3a3538d1bfd20217a7ed72d950f6d9e4857d
Kim Young Jin <kimyoungjin1001@gmail.com>
2025-01-09 13:11:12 +0900

    remove complexity

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,32 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { useUser } from "@clerk/clerk-react";
 import { GET_USER_SUBSCRIPTION } from "./data";
-import { useEffect } from "react";
 
 export function useSubscription() {
-  const { user, isLoaded } = useUser();
-  console.log("User: ", user);
-  console.log("Is user loaded?: ", isLoaded);
+  const { user } = useUser();
 
   const { data, loading, refetch } = useQuery(GET_USER_SUBSCRIPTION, {
     variables: { userId: user?.id },
     skip: !user?.id,
-    onError: (error) => {
-      console.error("Subscription query error:", error);
-    },
   });
 
-  // Set up effect to refetch when user becomes available
-  useEffect(() => {
-    if (isLoaded && user?.id) {
-      console.log("User loaded, refetching subscription status");
-      refetch();
-    }
-  }, [isLoaded, user?.id, refetch]);
-
   const hasActiveSubscription = data?.subscriptions?.[0]?.status === "active";
-  console.log("Use subscription data: ", data);
-  console.log("hasActiveSubscription", hasActiveSubscription);
 
   return {
     hasActiveSubscription,
