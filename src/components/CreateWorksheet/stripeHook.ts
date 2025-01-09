162fbb5827e6f13c9b5889b73fa9fb18786cf933
Kim Young Jin <kimyoungjin1001@gmail.com>
2025-01-09 12:40:07 +0900

    fix hook again

--- src/components/CreateWorksheet/stripeHook.ts
+++ src/components/CreateWorksheet/stripeHook.ts
@@ -1,28 +1,24 @@
 import { useState, useEffect } from "react";
 import { useSearch } from "wouter";
 import qs from "qs";
-import { useQuery } from "@apollo/client";
+import { useApolloClient, useQuery } from "@apollo/client";
 import { GET_USER_SUBSCRIPTION } from "../../hooks/data";
-import { useUser } from "@clerk/clerk-react";
 
 export function useStripeReturn() {
   const searchParams = useSearch();
   const [showSuccessModal, setShowSuccessModal] = useState(false);
-  const { user } = useUser();
-  const { refetch } = useQuery(GET_USER_SUBSCRIPTION, {
-    variables: { userId: user?.id },
-    skip: !user?.id,
-  });
+  const client = useApolloClient();
 
   useEffect(() => {
     const params = qs.parse(searchParams, { ignoreQueryPrefix: true });
     if (params["subscription-success"] === "true") {
-      console.log("subscription-success is true");
       setShowSuccessModal(true);
       // Refetch subscription status when success is true
-      refetch();
+      client.refetchQueries({
+        include: [GET_USER_SUBSCRIPTION],
+      });
     }
-  }, [searchParams, refetch]);
+  }, [searchParams, client]);
 
   return { showSuccessModal, setShowSuccessModal };
 }
