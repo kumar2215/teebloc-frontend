f874e6548b4569b6a8bebf51b98bf967684d1d7f
Kim Young Jin <kimyoungjin1001@gmail.com>
2025-05-13 15:22:30 +0800

    allow admin to subscribe

--- src/hooks/useSubscription.ts
+++ src/hooks/useSubscription.ts
@@ -1,16 +1,19 @@
 import { useQuery } from "@apollo/client";
 import { useUser } from "@clerk/clerk-react";
 import { GET_USER_SUBSCRIPTION } from "./data";
+import { useIsAdmin } from "./useIsAdmin";
 
 export function useSubscription() {
   const { user } = useUser();
+  const isAdmin = useIsAdmin();
 
   const { data, loading, refetch } = useQuery(GET_USER_SUBSCRIPTION, {
     variables: { userId: user?.id },
     skip: !user?.id,
   });
 
-  const hasActiveSubscription = data?.subscriptions?.[0]?.status === "active";
+  const hasActiveSubscription =
+    data?.subscriptions?.[0]?.status === "active" || isAdmin;
 
   return {
     hasActiveSubscription,
