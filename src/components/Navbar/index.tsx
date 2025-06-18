49bc1452aca99bdb7e58d446e55ef45fd9031f3c
Kumar2215 <kumar.kg279@gmail.com>
2025-06-18 11:14:27 +0800

    Add button under Profile dropdown to open generate invite modal

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,6 +13,7 @@ import { cartItemsVar } from "../CreateWorksheet/data.tsx";
 import posthog from "posthog-js";
 import { useState, useEffect, useRef } from "react";
 import { useSubscription } from "../../hooks/useSubscription.ts";
+import CreateInviteModal from "../Invite/index.tsx";
 
 export default function Navbar() {
   const { signOut } = useClerk();
@@ -45,17 +46,17 @@ export default function Navbar() {
   }, [menuOpen]);
 
   return (
-    <div className="p-4 sticky top-0 z-10">
+    <div className="sticky top-0 z-10 p-4">
       <div className="navbar bg-primary rounded-box">
-        <div className="navbar-start flex items-center">
+        <div className="flex items-center navbar-start">
           <Link href="/practice">
-            <div className="text-xl mr-2">Teebloc</div>
+            <div className="mr-2 text-xl">Teebloc</div>
           </Link>
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost m-1">
+            <label tabIndex={0} className="m-1 btn btn-ghost">
               {location.startsWith("/practice") ? "Practice" : "Writing"}
               <svg
-                className="fill-current h-4 w-4 ml-1"
+                className="w-4 h-4 ml-1 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
@@ -92,7 +93,7 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div className="flex sm:hidden ml-auto relative" ref={menuRef}>
+        <div className="relative flex ml-auto sm:hidden" ref={menuRef}>
           <button
             className="btn btn-square btn-ghost"
             onClick={() => setMenuOpen(!menuOpen)}
@@ -101,7 +102,7 @@ export default function Navbar() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
+              className="inline-block w-5 h-5 stroke-current"
             >
               <path
                 strokeLinecap="round"
@@ -112,7 +113,7 @@ export default function Navbar() {
             </svg>
           </button>
           {menuOpen && (
-            <div className="absolute top-full right-0 mt-2 menu bg-primary rounded-box w-44 shadow-lg space-y-2">
+            <div className="absolute right-0 mt-2 space-y-2 shadow-lg top-full menu bg-primary rounded-box w-44">
               <NavItems
                 location={location}
                 cartItems={cartItems}
@@ -122,7 +123,7 @@ export default function Navbar() {
           )}
         </div>
 
-        <div className="navbar-end space-x-2 hidden sm:flex">
+        <div className="hidden space-x-2 navbar-end sm:flex">
           <NavItems
             location={location}
             cartItems={cartItems}
@@ -149,6 +150,7 @@ function NavItems({
   const practiceMatch = location.startsWith("/practice");
   const { hasActiveSubscription, loading } = useSubscription();
   const [isPortalLoading, setIsPortalLoading] = useState(false);
+  const [openInviteModal, setOpenInviteModal] = useState(false);
 
   // Modified from https://clerk.com/docs/backend-requests/making/cross-origin
   const authenticatedFetch = async (url: string, options?: RequestInit) => {
@@ -222,6 +224,11 @@ function NavItems({
           </>
         )}
 
+        <CreateInviteModal
+          isOpen={openInviteModal}
+          onClose={() => setOpenInviteModal(false)}
+        />
+
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-outline">
             Profile
@@ -264,6 +271,11 @@ function NavItems({
                 </a>
               </li>
             )}
+            <li>
+              <button onClick={() => setOpenInviteModal(true)}>
+                Generate invite link
+              </button>
+            </li>
             <li>
               <a onClick={onSignOut}>Sign out</a>
             </li>
