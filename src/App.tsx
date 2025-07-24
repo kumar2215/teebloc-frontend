53dee463c6120ce4874897f17f086fab2342001b
Kumar2215 <kumar.kg279@gmail.com>
2025-07-24 13:03:47 +0800

    Clean up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { SignedIn } from "@clerk/clerk-react";
+import { SignedIn, useUser } from "@clerk/clerk-react";
 import { Redirect, Route, Switch, useRoute } from "wouter";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import MyWorksheets from "./components/MyWorksheets";
 import Navbar from "./components/Navbar";
 import Options from "./components/Options";
-import { useUser } from "@clerk/clerk-react";
 import posthog from "posthog-js";
 import Writing from "./components/writing/Writing";
 import Feedback from "./components/writing/Feedback";
